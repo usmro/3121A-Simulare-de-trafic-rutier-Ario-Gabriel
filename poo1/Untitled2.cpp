7529af95990844552d9a63baa8b70fd05483ea08
Skynight112 <skynight254@gmail.com>
2026-04-15 16:53:40 +0300

    Day 1 - Street, Intersection, RoadNetwork classes

---
+++ poo1/Untitled2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using std::string;
+
+class Student{
+public:
+    string Nume;
+    string Clasa;
+    int ani;
+
+
+};
+
+int main()
+{
+    Student student1;
+    student1.Nume="Andrei";
+    student1.Clasa="V";
+    student1.ani= 2;
+
+}
+
